348a8f4be29741de9c4fdc344f096a19dbf04762
kaushal2469 <kaushal2469@gmail.com>
2024-08-27 14:43:45 +0530

    create z-button component

---
+++ libs/zein/src/lib/zein/components/button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'z-button',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss',
+})
+export class ButtonComponent {}
