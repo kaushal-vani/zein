cd8d6303cf3376d29126acce7073f351dad86574
kaushal2469 <kaushal2469@gmail.com>
2024-08-27 15:30:00 +0530

    zein input error component

---
+++ libs/zein/src/lib/zein/components/forms/input error/input error.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'z-input-error',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './input error.component.html',
+  styleUrl: './input error.component.scss',
+})
+export class InputErrorComponent {}
