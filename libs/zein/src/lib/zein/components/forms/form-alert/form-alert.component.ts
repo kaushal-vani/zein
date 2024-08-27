57e908dc274a5d39e471595ee5c2a4eebb4494c7
kaushal2469 <kaushal2469@gmail.com>
2024-08-27 15:29:31 +0530

    zein form-alert component

---
+++ libs/zein/src/lib/zein/components/forms/form-alert/form-alert.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'z-form-alert',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './form-alert.component.html',
+  styleUrl: './form-alert.component.scss',
+})
+export class FormAlertComponent {}
