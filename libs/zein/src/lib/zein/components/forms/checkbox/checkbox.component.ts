82ecd89dd053d5e51abaa82998725716b75735c2
kaushal2469 <kaushal2469@gmail.com>
2024-08-27 15:28:03 +0530

    zein checkbox component

---
+++ libs/zein/src/lib/zein/components/forms/checkbox/checkbox.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'z-checkbox',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './checkbox.component.html',
+  styleUrl: './checkbox.component.scss',
+})
+export class CheckboxComponent {}
