5bbd28db73d9a76a099b4a8041bb7839c8110040
kaushal2469 <kaushal2469@gmail.com>
2024-08-27 15:32:21 +0530

    zein radio-button component

---
+++ libs/zein/src/lib/zein/components/forms/radio-button/radio-button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'z-radio-button',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './radio-button.component.html',
+  styleUrl: './radio-button.component.scss',
+})
+export class RadioButtonComponent {}
