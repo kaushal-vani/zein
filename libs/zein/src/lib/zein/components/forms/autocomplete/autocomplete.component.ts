d3318bcabc8aaed09dbd4083119d31f82ee0bb0a
kaushal2469 <kaushal2469@gmail.com>
2024-08-27 15:26:41 +0530

    zein autocomplete component

---
+++ libs/zein/src/lib/zein/components/forms/autocomplete/autocomplete.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'z-autocomplete',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './autocomplete.component.html',
+  styleUrl: './autocomplete.component.scss',
+})
+export class AutocompleteComponent {}
