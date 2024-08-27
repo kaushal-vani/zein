4a1eefd5519636150432f929547963d716f21e86
kaushal2469 <kaushal2469@gmail.com>
2024-08-27 15:23:05 +0530

    update button types

---
+++ libs/zein/src/lib/zein/types/button.types.ts
@@ -0,0 +1,10 @@
+export type ButtonType = 'button' | 'submit' | 'reset';
+export type ButtonVariant = 'solid' | 'outlined' | 'text';
+export type ButtonColor =
+  | 'primary'
+  | 'secondary'
+  | 'danger'
+  | 'success'
+  | 'warning';
+export type ButtonSize = 'small' | 'medium' | 'large';
+export type ButtonShape = 'rounded' | 'square' | 'pill';
