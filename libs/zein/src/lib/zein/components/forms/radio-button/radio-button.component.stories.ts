a81cd6c3f100a5b633eb4de4e95ae7688167bfee
kaushal2469 <kaushal2469@gmail.com>
2024-08-31 01:52:28 +0530

    update radio button

--- libs/zein/src/lib/zein/components/forms/radio-button/radio-button.component.stories.ts
+++ libs/zein/src/lib/zein/components/forms/radio-button/radio-button.component.stories.ts
@@ -1,24 +1,36 @@
 import type { Meta, StoryObj } from '@storybook/angular';
 import { RadioButtonComponent } from './radio-button.component';
 
-import { within } from '@storybook/testing-library';
-import { expect } from '@storybook/jest';
-
 const meta: Meta<RadioButtonComponent> = {
   component: RadioButtonComponent,
   title: 'Forms/RadioButtonComponent',
 };
 export default meta;
+
 type Story = StoryObj<RadioButtonComponent>;
 
-export const Primary: Story = {
-  args: {},
+export const Default: Story = {
+  args: {
+    label: 'Option 1',
+    name: 'options',
+    value: 'option1',
+  },
 };
 
-export const Heading: Story = {
-  args: {},
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement);
-    expect(canvas.getByText(/zein works!/gi)).toBeTruthy();
+export const Checked: Story = {
+  args: {
+    label: 'Option 2',
+    name: 'options',
+    value: 'option2',
+    checked: true,
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    label: 'Disabled Option',
+    name: 'options',
+    value: 'option3',
+    disabled: true,
   },
 };
--- libs/zein/src/lib/zein/components/forms/radio-button/radio-button.component.ts
+++ libs/zein/src/lib/zein/components/forms/radio-button/radio-button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,4 +8,18 @@ import { CommonModule } from '@angular/common';
   templateUrl: './radio-button.component.html',
   styleUrl: './radio-button.component.scss',
 })
-export class RadioButtonComponent {}
+export class RadioButtonComponent {
+  @Input() label = '';
+  @Input() name = '';
+  @Input() value = '';
+  @Input() checked = false;
+  @Input() disabled = false;
+
+  @Output() valueChange: EventEmitter<string> = new EventEmitter<string>();
+
+  onChange(): void {
+    if (!this.disabled) {
+      this.valueChange.emit(this.value);
+    }
+  }
+}
